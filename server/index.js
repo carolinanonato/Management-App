7685b0d24550216b0c2ca33c8db8d7dac119a1f0
Carolina Nonato <csnonato@gmail.com>
2022-09-01 21:19:17 -0400

    Create express server

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.listen(port, console.log(`Server started on port ${port}`));
